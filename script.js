ec21cdeb2f122602277d7ee18605aa50d32ceea7
PuckerFace <113001532+PuckerFace@users.noreply.github.com>
2024-11-24 17:30:22 +0100

    some changes

--- script.js
+++ script.js
@@ -19,8 +19,23 @@ let day = date.getDate();
 let month = 1 + date.getMonth();
 let year = date.getFullYear();
 
+// To calculate leap year
+
+const leapYear = (y) => {
+  let isLeapYear = true;
+  if (y % 100 === 0 ? y % 400 === 0 : y % 4 === 0) {
+    isLeapYear = true;
+  } else {
+    isLeapYear = false;
+  }
+  return isLeapYear;
+};
+// console.log(leapYear(2023));
+
 const monthDate = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 // const error = document.querySelector('.err');
+
+//Validation
 
 const validate = () => {
   let validator = true;
@@ -28,26 +43,46 @@ const validate = () => {
 
   allInputs.forEach((input) => {
     const parent = input.parentElement;
-    if (!input.value) {
-      input.style.borderColor = 'hsl(0, 100%, 67%)';
 
+    if (!input.value) {
+      //empty field
+      input.style.borderColor = 'hsl(0, 100%, 67%)';
       parent.querySelector('.err').innerText = 'this field is required.';
       validator = false;
+    } else if (dayInput.value > 31) {
+      //invalid day
+      dayInput.style.borderColor = 'hsl(0, 100%, 67%)';
+      dayError.innerText = 'must be valid day.';
+      validator = false;
     } else if (monthInput.value > 12) {
-      monthInput.style.borderColor = 'red';
+      //invalid month
+      monthInput.style.borderColor = 'hsl(0, 100%, 67%)';
       monthError.innerText = 'must be valid month.';
       validator = false;
-    } else if (dayInput.value > 31) {
-      dayInput.style.borderColor = 'red';
+    } else if (dayInput.value > 29 && monthInput.value == 2) {
+      //invalid day for february
+      dayInput.style.borderColor = 'hsl(0, 100%, 67%)';
       dayError.innerText = 'must be valid day.';
       validator = false;
     } else if (yearInput.value > year) {
-      yearInput.style.borderColor = 'red';
+      //For future years or invalid year
+      yearInput.style.borderColor = 'hsl(0, 100%, 67%)';
       yearError.innerText = 'Not Born Yet.';
       validator = false;
+    } else if (
+      dayInput.value == 29 &&
+      monthInput.value == 2 &&
+      leapYear(yearInput.value) === false
+    ) {
+      //Leap year
+      yearInput.style.borderColor = 'hsl(0, 100%, 67%)';
+      yearError.innerText = 'Not a leap year.';
+      validator = false;
     } else {
+      //no error
       input.style.borderColor = 'hsl(0, 0%, 8%)';
       parent.querySelector('.err').innerText = '';
+
       validator = true;
     }
   });
